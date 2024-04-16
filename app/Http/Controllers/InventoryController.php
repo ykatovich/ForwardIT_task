07b5fe0cb0edccb9fcc93a5762f6ac728dcad6b4
Yuri Katovich <yuroff19@gmail.com>
2024-04-16 19:18:14 +0300

    FOIT-000 Clean up

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreInventoryRequest;
 use App\Models\Inventory;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 
 class InventoryController extends Controller
 {
-    public function index()
+    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         $inventories = Inventory::all();
         return view('inventory.list', compact('inventories'));
     }
 
-    public function create()
+    public function create(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         return view('inventory.create');
     }
 
-    public function store(StoreInventoryRequest $request)
+    public function store(StoreInventoryRequest $request): RedirectResponse
     {
         $validatedData = $request->validated();
 
@@ -27,7 +31,7 @@ class InventoryController extends Controller
         return redirect()->route('inventory.index');
     }
 
-    public function delete(Inventory $item)
+    public function delete(Inventory $item): RedirectResponse
     {
         $item->delete();
 
